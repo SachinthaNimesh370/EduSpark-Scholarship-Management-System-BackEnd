c365e671334ab16cb7820a966cae54f4d8f75c74
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-04-12 19:46:54 +0530

    Initialize Spring boot Project For EduSpark

---
+++ src/main/java/lk/ac/jfn/uoj/EduSpark/EduSparkApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.jfn.uoj.EduSpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/jfn/uoj/EduSpark/EduSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.jfn.uoj.EduSpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
