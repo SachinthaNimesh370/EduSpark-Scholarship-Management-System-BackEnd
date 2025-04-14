e4c5be5bdff2c5388f0d79e537a0b551a65f2acc
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-04-14 12:50:12 +0530

    implement User Entity class

---
+++ src/main/java/lk/ac/jfn/uoj/EduSpark/entity/UserEntity.java
@@ -0,0 +1,15 @@
+package lk.ac.jfn.uoj.EduSpark.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class UserEntity {
+    @Id
+    int id;
+    String name;
+    String email;
+    String password;
+}
