745443e0d294f5c08845a2db4be9aecaa790366f
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-04-14 12:30:19 +0530

    Connect MySQL database with server

--- src/main/java/lk/ac/jfn/uoj/EduSpark/controller/HomeController.java
+++ src/main/java/lk/ac/jfn/uoj/EduSpark/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/home")
+@RequestMapping("/home")
 public class HomeController {
     private final JWTService jwtService;
 
