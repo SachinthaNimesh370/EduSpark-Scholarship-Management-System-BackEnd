74766477d274094fc439fdfeeda63f9617483977
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-04-14 14:39:26 +0530

    password encoder using secure password

--- src/main/java/lk/ac/jfn/uoj/EduSpark/repo/UserRepository.java
+++ src/main/java/lk/ac/jfn/uoj/EduSpark/repo/UserRepository.java
@@ -3,5 +3,10 @@ package lk.ac.jfn.uoj.EduSpark.repo;
 import lk.ac.jfn.uoj.EduSpark.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
+
+    Optional<UserEntity> findByName(String name);
+
 }
