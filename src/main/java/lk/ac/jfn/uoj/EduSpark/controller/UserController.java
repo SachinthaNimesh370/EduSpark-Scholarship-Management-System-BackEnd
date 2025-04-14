c065c3a1335ca23f5252373a78e79c8b0806553a
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-04-14 13:13:14 +0530

    implement user registation work flow primary level

---
+++ src/main/java/lk/ac/jfn/uoj/EduSpark/controller/UserController.java
@@ -0,0 +1,24 @@
+package lk.ac.jfn.uoj.EduSpark.controller;
+
+import lk.ac.jfn.uoj.EduSpark.DTO.UserDTO;
+import lk.ac.jfn.uoj.EduSpark.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/signup")
+    public String signUp(@RequestBody UserDTO userDTO){
+        userService.signUp(userDTO);
+        return "";
+    }
+}
